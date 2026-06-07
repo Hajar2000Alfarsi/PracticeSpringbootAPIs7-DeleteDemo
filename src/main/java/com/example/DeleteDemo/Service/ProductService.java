51dded74602d6a0ccac73667fa1c97c9b379aa28
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 15:29:19 +0400

    Add 3 products

--- src/main/java/com/example/DeleteDemo/Service/ProductService.java
+++ src/main/java/com/example/DeleteDemo/Service/ProductService.java
@@ -1,4 +1,19 @@
 package com.example.DeleteDemo.Service;
 
+import com.example.DeleteDemo.Entites.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+
+@Service
 public class ProductService {
+    private HashMap<Integer, Product> productMap = new HashMap<>();
+
+    public void loadProducts() {
+        productMap.put(1, new Product(1, "Laptop", 500));
+        productMap.put(2, new Product(2, "Phone", 300));
+        productMap.put(3, new Product(3, "Tablet", 200));
+    }
+
+
 }
