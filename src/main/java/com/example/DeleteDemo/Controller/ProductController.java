08a751594b65249d3727b59fc9b68ec684ee0fe4
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 15:48:13 +0400

    controller delelte

--- src/main/java/com/example/DeleteDemo/Controller/ProductController.java
+++ src/main/java/com/example/DeleteDemo/Controller/ProductController.java
@@ -1,4 +1,18 @@
 package com.example.DeleteDemo.Controller;
 
+import com.example.DeleteDemo.Service.ProductService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ProductController {
+
+    ProductService productService = new ProductService();
+
+    @DeleteMapping("/products/{id}")
+    public String deleteProduct(@PathVariable int productId){
+        return productService.deleteProductById(productId);
+    }
 }
