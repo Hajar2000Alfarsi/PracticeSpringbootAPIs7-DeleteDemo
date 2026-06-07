5f38cc7211823f8de5a1e62312e4a34e1f387a52
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 15:18:11 +0400

    add dependencies

---
+++ src/main/java/com/example/DeleteDemo/DeleteDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.DeleteDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeleteDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeleteDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DeleteDemo/DeleteDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DeleteDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeleteDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
