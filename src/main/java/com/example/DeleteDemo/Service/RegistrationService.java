39d49131711c7d215400f958696122dfa68250d8
Hajar <hajar2000alfarsi@gmail.com>
2026-06-07 16:11:23 +0400

    Initialize sample data

--- src/main/java/com/example/DeleteDemo/Service/RegistrationService.java
+++ src/main/java/com/example/DeleteDemo/Service/RegistrationService.java
@@ -1,5 +1,23 @@
 package com.example.DeleteDemo.Service;
 
-public class RegistrationService {
+import com.example.DeleteDemo.Entites.EventRegistration;
+import jakarta.annotation.PostConstruct;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class RegistrationService {
+    private Map<Integer, EventRegistration> registrations = new HashMap<>();
+
+    public RegistrationService() {
+        registrations.put(101, new EventRegistration(101, "Ahmed"));
+        registrations.put(102, new EventRegistration(102, "Sara"));
+        registrations.put(103, new EventRegistration(103, "John"));
+        registrations.put(104, new EventRegistration(104, "Ali"));
+    }
+
+    public String deleteMultipleByIds(List<Integer> ids){
+
+    }
 }
